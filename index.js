c17a0347f0b0bf11e150f08230de54a8d0b0f749
Ridwanul Hoque <rhr06junhoque@gmail.com>
2025-04-07 02:05:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const express =  require('express');
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8gt7g.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const usersCollection = client.db("PawkieDB").collection("users")
+
+
+     // users
+
+     app.get('/users', async (req, res) => {
+        console.log(req.headers)
+        const result = await usersCollection.find().toArray();
+        res.send(result)
+    });
+    
+
+    app.get('/users/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        if (email !== req.decoded.email) {
+            return res.status(403).send({ message: 'unauthorized access' })
+        }
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        let admin = false;
+        if (user) {
+            admin = user?.role === 'admin';
+
+        }
+        res.send({ admin })
+
+    })
+    app.patch('/users/admin/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) }
+        const updatedDoc = {
+            $set: {
+                role: 'admin'
+            }
+        }
+        const result = await usersCollection.updateOne(filter, updatedDoc)
+        res.send(result)
+    })
+    app.delete('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await usersCollection.deleteOne(query);
+        res.send(result)
+    })
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email }
+        const existingUser = await usersCollection.findOne(query)
+        if (existingUser) {
+            return res.send({ message: 'user already exists', insertedId: null })
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result)
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();/
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Online')
+})
+
+app.listen(port, () => {
+    console.log(`Online market is ready ${port}`);
+
+})
